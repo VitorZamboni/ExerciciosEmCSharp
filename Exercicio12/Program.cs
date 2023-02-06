935af101ec26d669be3faa3e96a35d48c6bc26e3
Vitor Mateus Zamboni <101102482+VitorZamboni@users.noreply.github.com>
2023-02-06 13:58:16 -0300

    Add files via upload

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("------ Gera uma matriz identidade da ordem que o usuario solicitar ------");
+Console.Write("Digite a ordem que queres a matriz: ");
+int ordem = Convert.ToInt32(Console.ReadLine());
+
+for (int linha = 1; linha <= ordem; linha++)
+{
+	for (int coluna = 0; coluna <= ordem; coluna++)
+	{
+		if (coluna == linha)
+		{
+			Console.Write("1 ");
+		}
+		else
+		{
+			Console.Write("0 ");
+		}
+
+	}
+	Console.WriteLine();
+}
